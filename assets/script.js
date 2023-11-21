040e32854b3f0de0f61d2b528640ae2f7806854f
emelinew1 <emeline.wulstecke@gmail.com>
2023-11-21 15:30:20 +0100

    Add slides

--- assets/script.js
+++ assets/script.js
@@ -18,21 +18,17 @@ const slides = [
 	}
 ]
 
-//------------------------- source & ajout des slides
-
-//variable qui sélectionne la classe banner-img
+// ------------------------- Variables
 
 const imageBanner = document.querySelector(".banner-img");
-
-// le chemin relatif +le nom de fichier du tableau, débutant à l'index 0 pour la première image.
-imageBanner.src = "assets/images/slideshow/" + slides[0].image;
-imageBanner.alt = slides[0].tagLine;
-
-
-//------------------------- Arrows
 const arrow_left = document.querySelector('.arrow_left');
 const arrow_right = document.querySelector('.arrow_right');
+const dots = document.querySelector('.dots');
+const text = document.getElementById("banner").querySelector("p");
+const numberOfDots = slides.length;
+let compteur = 0;
 
+//------------------------- Arrows
 
 arrow_left.addEventListener('click', () => {
 	console.log('Flèche gauche cliquée!');
@@ -42,25 +38,54 @@ arrow_right.addEventListener('click', () => {
 	console.log('Flèche droite cliquée!');
 });
 
-//--------------------------- Dots
 
-// variable initiale avant la boucle
-let compteur = 0;
+// le chemin relatif +le nom de fichier du tableau, débutant à l'index 0 pour la première image.
 
-//variable qui sélectionne la div avec la class dots
-const dots = document.querySelector('.dots');
+imageBanner.src = "assets/images/slideshow/" + slides[0].image;
+imageBanner.alt = slides[0].tagLine;
 
-// Ajout de 4 divs ".dot" à la div parente ".dots"
-for (let i = 0; i < 4; i++) {
+// ------------------------- Dots
+
+//Ajout de 4 divs ".dot" à la div parente ".dots"
+//For indique le nombre de dots,tant que i est inférieur à la taille du tableau, on ajoute +1 à i
+
+for (let i = 0; i < numberOfDots; i++) {
 	const dotElement = document.createElement('div');
 	dotElement.classList.add('dot');
 	dots.appendChild(dotElement);
+  
+	if (i === compteur) dotElement.classList.add("dot_selected");
 
-	if (i == compteur) dotElement.classList.add("dot_selected");
-}
+// -------------- les dots changes quand on clique sur les fleche ou sur les dots
+	dotElement.addEventListener('click', () => {
+	  changeSlide(i);
+	});
+  }
+  
+ //-------------- changement d'images et texte au clic
+  arrow_left.addEventListener('click', () => {
+	compteur = (compteur - 1 + slides.length) % slides.length;
+	changeSlide(compteur);
+  });
+  
+  arrow_right.addEventListener('click', () => {
+	compteur = (compteur + 1) % slides.length;
+	changeSlide(compteur);
+  });
+  
 
-
-
+  function changeSlide(index) {
+	imageBanner.src = `assets/images/slideshow/${slides[index].image}`;
+	text.innerHTML = slides[index].tagLine;
+  
+	const allDots = dots.children;
+	for (let i = 0; i < allDots.length; i++) {
+	  allDots[i].classList.toggle("dot_selected", i === index);
+	}
+  
+	compteur = index;
+	console.log("Changement de diapositive réussi!");
+  }
 
 
 
